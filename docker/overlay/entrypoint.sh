16ece3e1420d49153716319c683fd6e44b4be5dd
Josh.5 <jsunnex@gmail.com>
2026-02-07 13:23:16 +1300

    Improve our healthcheck on the VPN connection

--- docker/overlay/entrypoint.sh
+++ docker/overlay/entrypoint.sh
@@ -39,6 +39,50 @@ print_log() {
 }
 
 ################################################
+# --- IP helpers
+#
+_ip_sources=(
+    "https://ifconfig.co/ip"
+    "https://ipinfo.io/ip"
+    "https://api.ipify.org"
+)
+
+_is_ipv4() {
+    local ip="${1:-}"
+    [[ "${ip}" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]] || return 1
+    IFS='.' read -r o1 o2 o3 o4 <<< "${ip}"
+    for o in "${o1}" "${o2}" "${o3}" "${o4}"; do
+        [ "${o}" -le 255 ] || return 1
+    done
+}
+
+_fetch_manager_ip() {
+    local url ip
+    for url in "${_ip_sources[@]}"; do
+        ip=$(curl -4 -fsS --max-time 10 "${url}" 2>/dev/null || true)
+        ip=$(echo "${ip}" | tr -d ' \r\n')
+        if _is_ipv4 "${ip}"; then
+            echo "${ip}"
+            return 0
+        fi
+    done
+    return 1
+}
+
+_fetch_proxy_ip() {
+    local url ip
+    for url in "${_ip_sources[@]}"; do
+        ip=$(${docker_compose_cmd:?} exec -T proxy sh -c "curl -4 -fsS --max-time 10 \"${url}\"" 2>/dev/null || true)
+        ip=$(echo "${ip}" | tr -d ' \r\n')
+        if _is_ipv4 "${ip}"; then
+            echo "${ip}"
+            return 0
+        fi
+    done
+    return 1
+}
+
+################################################
 # --- Create TERM monitor
 #
 _term() {
@@ -77,6 +121,17 @@ for start_script in /init.d/start/*.sh; do
     fi
 done
 
+VPN_HEALTH_STARTUP_GRACE="${VPN_HEALTH_STARTUP_GRACE:-60}"
+VPN_HEALTH_INTERVAL="${VPN_HEALTH_INTERVAL:-60}"
+VPN_HEALTH_MAX_FAILS="${VPN_HEALTH_MAX_FAILS:-3}"
+VPN_HEALTH_FAIL_ACTION="${VPN_HEALTH_FAIL_ACTION:-restart-stack}"
+VPN_HEALTH_HOST_IP_REFRESH="${VPN_HEALTH_HOST_IP_REFRESH:-300}"
+
+host_ip=""
+host_ip_last_refresh=0
+vpn_fail_count=0
+vpn_health_next_check_ts=0
+
 ################################################
 # --- Create compose stack monitor
 #
@@ -84,6 +139,12 @@ _stack_monitor() {
     print_log info "Waiting for child services to exit"
     cd /config/${STACK_NAME:?}/
     while true; do
+        now_ts=$(date +%s)
+        if [ "${vpn_health_next_check_ts:?}" -eq 0 ]; then
+            vpn_health_next_check_ts=$((now_ts + VPN_HEALTH_STARTUP_GRACE))
+            print_log info "Waiting ${VPN_HEALTH_STARTUP_GRACE:?}s before VPN health enforcement..."
+        fi
+
         # Check if any service has exited with a non-zero status code
         print_log info "Check if any service has exited with a non-zero status code"
         exited_services=$(${docker_compose_cmd:?} ps --all --filter "status=exited" | grep -v "^NAME" || true)
@@ -113,14 +174,59 @@ _stack_monitor() {
             esac
         done
 
-        print_log info "Checking HLS Proxy external IP..."
-        external_ip=$(${docker_compose_cmd:?} exec proxy curl -4 -s -m 10 ifconfig.co || true)
-        if [ "X${external_ip:-}" != "X" ]; then
-            print_log info "  - Current IP: ${external_ip:-}"
-        else
-            print_log info "  - Unable to fetch current IP ${external_ip:-} from container proxy..."
+        if [ -z "${host_ip:-}" ] || [ $((now_ts - host_ip_last_refresh)) -ge "${VPN_HEALTH_HOST_IP_REFRESH:?}" ]; then
+            print_log info "Checking host external IP..."
+            host_ip=$(_fetch_manager_ip || true)
+            if [ -n "${host_ip:-}" ]; then
+                print_log info "  - Host IP: ${host_ip:-}"
+                host_ip_last_refresh=${now_ts:?}
+            else
+                print_log info "  - Unable to fetch host IP. Will retry."
+            fi
         fi
-        sleep 60 &
+
+        if [ "${now_ts:?}" -lt "${vpn_health_next_check_ts:?}" ]; then
+            print_log info "Skipping VPN health check until grace period ends."
+            sleep ${VPN_HEALTH_INTERVAL:?} &
+            wait $!
+            echo
+            continue
+        fi
+
+        print_log info "Checking VPN external IP..."
+        vpn_ip=$(_fetch_proxy_ip || true)
+        if [ -z "${vpn_ip:-}" ]; then
+            print_log error "  - Unable to fetch VPN IP from proxy container."
+            vpn_fail_count=$((vpn_fail_count + 1))
+        elif [ -n "${host_ip:-}" ] && [ "${vpn_ip:-}" = "${host_ip:-}" ]; then
+            print_log error "  - VPN IP matches host IP (${vpn_ip:-}). Leak suspected."
+            vpn_fail_count=$((vpn_fail_count + 1))
+        else
+            print_log info "  - VPN IP: ${vpn_ip:-}"
+            vpn_fail_count=0
+        fi
+
+        if [ "${vpn_fail_count:?}" -ge "${VPN_HEALTH_MAX_FAILS:?}" ]; then
+            print_log error "VPN health check failed ${vpn_fail_count:?} times. Action: ${VPN_HEALTH_FAIL_ACTION:?}"
+            case "${VPN_HEALTH_FAIL_ACTION:?}" in
+            exit)
+                exit 123
+                ;;
+            restart-stack)
+                print_log error "Restarting stack due to VPN health failure"
+                ${docker_compose_cmd:?} down --remove-orphans || true
+                ${docker_compose_cmd:?} up --detach --remove-orphans || true
+                vpn_fail_count=0
+                vpn_health_next_check_ts=$(( $(date +%s) + VPN_HEALTH_STARTUP_GRACE ))
+                ;;
+            *)
+                print_log error "Unknown VPN_HEALTH_FAIL_ACTION '${VPN_HEALTH_FAIL_ACTION:?}'. Exiting."
+                exit 123
+                ;;
+            esac
+        fi
+
+        sleep ${VPN_HEALTH_INTERVAL:?} &
         wait $!
         echo
     done
